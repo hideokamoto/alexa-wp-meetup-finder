62520612e02a9ba9538b72965e26630ccf559d94
hideokamoto <hide.okamoto@digitalcube.jp>
2019-05-28 11:53:37 +0900

    refactor contnet builder

--- lambda/custom/handlers/SearchMeetupHandler.ts
+++ lambda/custom/handlers/SearchMeetupHandler.ts
@@ -9,12 +9,80 @@ import {
 } from 'ask-utils'
 import moment from 'moment-timezone'
 
-import ResponseFactory from '../libs/responseFactory'
+import ResponseFactory, { IResponseBuilder, IResponseContent } from '../libs/responseFactory'
 import MeetupFinder from '../libs/meetupFinder'
 import List from '../libs/apl/List'
 import ListTemplateMetadataFactory from '../libs/apl/ListMetadata'
 import ListItem from '../libs/apl/ListItem'
 import { getLocation, getTimezoneByLocale, getRandomSpeechconTexts } from '../libs/helpers'
+import { TReqsponseBody } from '../model'
+
+class ContentBuilder {
+  private locale: string
+  private builder: IResponseBuilder
+  constructor(locale: string, builder: IResponseBuilder = ResponseFactory.init()) {
+    this.locale = locale
+    this.builder = builder
+  }
+  getProgressComment(): string {
+    return getRandomSpeechconTexts([
+      'オーケーです',
+      'オーケー',
+      'オッケー',
+      'ラジャー'
+    ])
+  }
+  getFailedToGetEventResponse(): IResponseContent {
+    return this.builder.putSpeechParagraph('データの取得に失敗しました。もう一度探したい地域名をお聞かせください。')
+      .putRepromptText('他の地域も調べますか？終了する場合は、ストップと話しかけてください。')
+      .getResponse()
+  }
+  getNoEventResponse(region?: string): IResponseContent {
+    const preText = region ? `${region}の`: ''
+    const builder = this.builder.putSpeechParagraph(`${preText}近くで開催されるイベントが今の所ない様子です・・・。他の地域も調べますか？`)
+      .putRepromptText('他の地域も調べますか？終了する場合は、ストップと話しかけてください。')
+    if (region) builder.putCardTitle(`${region}のイベント`).putCardContent(`${region}で今後開催予定のWordPress Meetupは今の所ありません。`)
+    return builder.getResponse()
+  }
+  getLocale(): string {
+    return this.locale
+  }
+  getListEventResponse(data: TReqsponseBody): IResponseContent {
+    this.builder.putSpeechParagraph(`${data.location.description}の周辺で開催予定のイベントは、 次の${data.events.length}件です。`)
+    // APL系処理のセットアップ
+    const metadataBuilder = ListTemplateMetadataFactory.init()
+    const title = `${data.location.description}周辺のイベント情報`
+    metadataBuilder.setTitle(title).setShortTitle(`${data.location.description}周辺`)
+    this.builder.putCardTitle(title)
+    const directive = new List(metadataBuilder)
+
+    data.events.forEach((event, i) => {
+      const place = getLocation(event)
+      const date = moment(event.date).format('YYYY/MM/DD H時')
+      const p = [
+        `${place}で`,
+        `${date}に`,
+        `${event.title.replace(/Meetup/ig, 'ミートアップ')}`,
+        'が開催されます。'
+      ].join('、')
+      this.builder.putSpeechParagraph(p)
+      const number = i + 1
+      const Item = new ListItem(number, `event-${number}`)
+      Item.setPrimaryText(`${date} ~`)
+        .setSecondaryText(event.title)
+      directive.putListItem(Item.getItem())
+      this.builder.putCardContent(event.title)
+        .putCardContent(event.meetup)
+        .putCardContent(date + ' ~ ')
+        .putCardContent('URL: ' + event.meetup_url)
+    })
+    this.builder
+      .putSpeechParagraph('他の地域も調べますか？')
+      .putRepromptText('他の地域も調べますか？終了する場合は、ストップと話しかけてください。')
+      .setDirective(directive.getDirective())
+    return this.builder.getResponse()
+  }
+}
 
 export default {
   canHandle(handlerInput: HandlerInput): boolean {
@@ -22,15 +90,11 @@ export default {
   },
   async handle(handlerInput: HandlerInput): Promise<Response> {
     const { responseBuilder } = handlerInput
-    const builder = ResponseFactory.init()
-    await enqueueProgressiveResponse(handlerInput, getRandomSpeechconTexts([
-      'オーケーです',
-      'オーケー',
-      'オッケー',
-      'ラジャー'
-    ]))
-    const finder = new MeetupFinder()
     const locale = getLocale(handlerInput)
+    const contentBuilder = new ContentBuilder(locale)
+    const inprogressMessage = contentBuilder.getProgressComment()
+    await enqueueProgressiveResponse(handlerInput, inprogressMessage)
+    const finder = new MeetupFinder()
     if (locale) finder.setLocale(locale)
     const region = getSlotValue(handlerInput, 'region')
     if (!region) {
@@ -43,53 +107,25 @@ export default {
     }
     const data = await finder.setLocation(region).get()
     if (!data) {
-      return responseBuilder.speak('データの取得に失敗しました。もう一度探したい地域名をお聞かせください。')
-        .reprompt('他の地域も調べますか？終了する場合は、ストップと話しかけてください。')
+      const failedToGetResponse = contentBuilder.getFailedToGetEventResponse()
+      return responseBuilder.speak(failedToGetResponse.speechText)
+        .reprompt(failedToGetResponse.repromptText)
         .getResponse()
     }
     if (data.events.length < 1) {
-      return responseBuilder.speak('近くで開催されるイベントが今の所ない様子です・・・。他の地域も調べますか？')
-        .reprompt('他の地域も調べますか？終了する場合は、ストップと話しかけてください。')
-        .getResponse()
+      const noEventResponse = contentBuilder.getNoEventResponse()
+      const noEvent = responseBuilder.speak(noEventResponse.speechText)
+        .reprompt(noEventResponse.repromptText)
+      if (noEventResponse.cardTitle) noEvent.withSimpleCard(noEventResponse.cardTitle, noEventResponse.cardText)
+      return noEvent.getResponse()
     }
-    builder.putSpeechParagraph(`${data.location.description}の周辺で開催予定のイベントは、 次の${data.events.length}件です。`)
-    // APL系処理のセットアップ
-    const metadataBuilder = ListTemplateMetadataFactory.init()
-    const title = `${data.location.description}周辺のイベント情報`
-    metadataBuilder.setTitle(title).setShortTitle(`${data.location.description}周辺`)
-    builder.putCardTitle(title)
-    const directive = new List(metadataBuilder)
-
-    data.events.forEach((event, i) => {
-      const place = getLocation(event)
-      const date = moment(event.date).format('YYYY/MM/DD H時')
-      const p = [
-        `${place}で`,
-        `${date}に`,
-        `${event.title.replace(/Meetup/ig, 'ミートアップ')}`,
-        'が開催されます。'
-      ].join('、')
-      builder.putSpeechParagraph(p)
-      const number = i + 1
-      const Item = new ListItem(number, `event-${number}`)
-      Item.setPrimaryText(`${date} ~`)
-        .setSecondaryText(event.title)
-      directive.putListItem(Item.getItem())
-      builder.putCardContent(event.title)
-        .putCardContent(event.meetup)
-        .putCardContent(date + ' ~ ')
-        .putCardContent('URL: ' + event.meetup_url)
-    })
-    builder
-      .putSpeechParagraph('他の地域も調べますか？')
-      .putRepromptText('他の地域も調べますか？終了する場合は、ストップと話しかけてください。')
-    const { speechText, repromptText, cardText, cardTitle } = builder.getResponse()
+    const { speechText, repromptText, cardText, cardTitle, directive } = contentBuilder.getListEventResponse(data)
     responseBuilder.speak(speechText)
       .reprompt(repromptText)
       .withSimpleCard(cardTitle, cardText)
     const interfaces = getSupportedInterfaces(handlerInput)
-    if (interfaces["Alexa.Presentation.APL"]) {
-      responseBuilder.addDirective(directive.getDirective())
+    if (directive && interfaces["Alexa.Presentation.APL"]) {
+      responseBuilder.addDirective(directive)
     }
     return responseBuilder.getResponse()
   }
--- lambda/custom/libs/responseFactory.ts
+++ lambda/custom/libs/responseFactory.ts
@@ -1,12 +1,30 @@
 import { escapeXmlCharacters } from 'ask-sdk-core'
+import { Directive } from 'ask-sdk-model'
+export interface IResponseContent {
+  speechText: string
+  repromptText: string
+  cardTitle: string
+  cardText: string
+  directive?: Directive
+}
+export interface IResponseBuilder {
+  putSpeechParagraph(text: string): IResponseBuilder
+  putSpeechText(text: string): IResponseBuilder
+  putRepromptText(text: string): IResponseBuilder
+  putCardTitle(title: string): IResponseBuilder
+  putCardContent(content: string): IResponseBuilder
+  setDirective(directive: Directive): IResponseBuilder
+  getResponse(): IResponseContent
+}
 export default class ResponseFactory {
-  public static init() {
+  public static init(): IResponseBuilder {
     const response = {
       speechTexts: [''],
       repromptTexts: [''],
       cardTitle: '',
       cardTexts: ['']
     }
+    let directive: Directive | undefined
     return {
       putSpeechParagraph(text: string) {
         response.speechTexts.push(`<p>${escapeXmlCharacters(text)}</p>`)
@@ -28,12 +46,21 @@ export default class ResponseFactory {
         response.cardTexts.push(content)
         return this
       },
+      setDirective(props: Directive) {
+        directive = props
+        return this
+      },
       getResponse() {
-        return {
+        const content: IResponseContent = {
           speechText: response.speechTexts.join(' '),
           cardTitle: response.cardTitle,
           cardText: response.cardTexts.filter(content => content).join('\n'),
           repromptText: response.repromptTexts.join(' ')
+        }
+        if (!directive) return content
+        return {
+          ...content,
+          directive
         }
       }
     }
